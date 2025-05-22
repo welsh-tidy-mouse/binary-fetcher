9c176c55c0be590bb7fd4d8ccdd1445591f88154
David Buros <d.buros@monsieurbiz.com>
2025-05-22 18:06:28 +0200

    feat(console): better autoload discover

--- bin/binary-fetcher.php
+++ bin/binary-fetcher.php
@@ -1,14 +1,15 @@
 #!/usr/bin/env php
 <?php
 
-use WelshTidyMouse\BinaryFetcher\BinaryProvider\BunJsBinaryProvider;
-use WelshTidyMouse\BinaryFetcher\BinaryProvider\DartSassBinaryProvider;
-use WelshTidyMouse\BinaryFetcher\BinaryProvider\TailwindCssBinaryProvider;
 use WelshTidyMouse\BinaryFetcher\Console\ConsoleRunner;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$classMapFile = __DIR__ . '/../vendor/composer/autoload_classmap.php';
+if (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
+    $classMapFile = __DIR__ . '/../../../autoload_classmap.php';
+} else {
+    require_once __DIR__ . '/../vendor/autoload.php';
+    $classMapFile = __DIR__ . '/../vendor/composer/autoload_classmap.php';
+}
 
 if (!file_exists($classMapFile)) {
     throw new RuntimeException('Composer classmap file not found. Run "composer dump-autoload".');
